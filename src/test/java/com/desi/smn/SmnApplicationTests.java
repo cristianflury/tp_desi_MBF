dfa8d88e14241a8138ff0e993e03239f2365267f
Cristian Flury <cristianflury@gmail.com>
2022-11-09 20:31:04 -0300

    Subida Inicial del Proyecto

---
+++ src/test/java/com/desi/smn/SmnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desi.smn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
