d28c932ed523614e1286d77c41d27cbda4ee48da
Cristian Flury <cristianflury@gmail.com>
2022-11-10 21:28:16 -0300

    Agrego entity Pronostico y repositorio de Ciudad y Pronostico

--- src/main/java/com/desi/smn/service/CiudadService.java
+++ src/main/java/com/desi/smn/service/CiudadService.java
@@ -1,4 +1,4 @@
-package service;
+package com.desi.smn.service;
 
 public class CiudadService {
 
--- src/main/java/model/Ciudad.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public class Ciudad {
-	private long idCiudad;
-	private String nombre;
-}
